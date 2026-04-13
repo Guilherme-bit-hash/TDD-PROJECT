7542a5c53666ce62a949272c6d61d76f7e0310f6
Guilherme-bit-hash <guilhermecardoso0033@gmail.com>
2026-04-12 22:22:50 -0300

    Criando BD de produção

--- functional_testes.py
+++ functional_testes.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -11,29 +13,35 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self): 
-        ...
-        # Quando ela aperta enter, a página atualiza, e mostra a lista
-        # "1: Estudar testes funcionais" como um item da lista TODO
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        inputbox.send_keys('Estudar testes funcionais')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+
         self.check_for_row_in_list_table('1: Estudar testes funcionais')
-        
-        # Ainda existe uma caixa de texto convidando para adicionar outro item
-        # Ela digita: "Estudar testes de unidade"
-        inputbox = self.browser.find_element_by_id('id_new_item')  
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Estudar testes de unidade')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
         self.check_for_row_in_list_table('1: Estudar testes funcionais')
         self.check_for_row_in_list_table('2: Estudar testes de unidade')
-        ...
-    def check_for_row_in_list_table(self, row_text):
-         table = self.browser.find_element_by_id('id_list_table')
-         rows = table.find_elements_by_tag_name('tr')
-         self.assertIn(row_text, [row.text for row in rows])
+
 
 if __name__ == '__main__':
     unittest.main()
